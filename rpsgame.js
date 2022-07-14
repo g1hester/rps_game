c045c34b5d1bec1ade1f1cd57a788486c77df31d
glhester <greglhester@gmail.com>
2022-07-14 09:30:55 -0700

    added base js structure

--- rpsgame.js
+++ rpsgame.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const choices = ["rock", "paper", "scissors"];
+
+function computerPlay () {
+
+};
+
+function playRound (playerSelection, computerSelection){
+};
+
+function playerChoice (){
+    //get random choice for player
+}
+
+function computerChoice (){
+    //get random choice for computer
+}
