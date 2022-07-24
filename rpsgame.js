efdc511ce00500fd2be8fa0317624544f13a6040
g1hester <greglhester@gmail.com>
2022-07-24 10:15:34 -0700

    fixed playerSelection output to log correctly without triggering secondary user input prompt

--- rpsgame.js
+++ rpsgame.js
@@ -13,18 +13,17 @@ function computerChoice(){
 }
 
 function playerChoice (){
-   let input = prompt("Enter rock, paper, or scissors");
-   input = input.toLowerCase();
+    let input = prompt("Enter rock, paper, or scissors");
+    input = input.toLowerCase();//converts player's input into lowercase letters
     return input;
 }
-
-
 
 function playRound (playerSelection, computerSelection) {
     playerSelection = playerChoice();
     computerSelection = computerChoice();
+    console.log(playerSelection);//You must console.log "playerSelection" variable instead of playerChoice(), because running playerChoice() function will trigger another input prompt
     
-
+    //this is a test to ensure playerChoice is executing properly
     if (playerSelection === "rock") {
         return "Tie game!";
     } else {return "Try again, noob!";
